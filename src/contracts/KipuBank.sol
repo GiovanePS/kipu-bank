15b5eddaf6b15720be8742a62db7b7e9fc753a46
GiovanePS <giovanespace@gmail.com>
2025-10-27 16:09:39 -0300

    feat: add support to usdc

--- src/contracts/KipuBank.sol
+++ src/contracts/KipuBank.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 interface AggregatorV3Interface {
     function latestRoundData() external view returns (
@@ -17,6 +19,7 @@ interface AggregatorV3Interface {
 /// @dev I got 1-2-3-4-5-6-7-8 M`s in my bank account
 contract KipuBank is AccessControl {
 	using Math for uint256;
+	using SafeERC20 for IERC20;
 
 	/// =========================== ROLES ===========================
     /// @notice recovery role constant
@@ -28,7 +31,7 @@ contract KipuBank is AccessControl {
     uint256 public constant ETHER_WITHDRAW_LIMIT = 10 ether;
 
 	/// @notice Per-withdrawal limit in USDC with 6 decimals
-    uint256 public constant WITHDRAW_LIMIT_USDC = 1_000 * 1e6;
+    uint256 public constant USDC_WITHDRAW_LIMIT = 1_000 * 1e6;
 
     /// @notice oracle data freshness guard
     uint256 public constant MAX_ORACLE_DELAY = 3 hours;
@@ -39,11 +42,23 @@ contract KipuBank is AccessControl {
 	/// @notice decimals of the feed
     uint8 private immutable feedDecimals;
 
+	/// @notice ETH token
+	address public constant ETH = address(0);
+
+	/// @notice USDC token
+	address public immutable USDC;
+
     /// @notice Maximum bank capacity
-    uint256 public immutable MAX_BANK_CAP;
+    uint256 public immutable MAX_BANK_CAP_ETH;
+
+	/// @notice Maximum bank capacity in USDC
+	uint256 public immutable MAX_BANK_CAP_USDC;
 
     /// @notice Current bank capacity
-    uint256 public currentBankCap;
+    uint256 public currentBankCapEth;
+
+	/// @notice Current bank capacity in USDC
+	uint256 public currentBankCapUsdc;
 
     /// @notice Total number of deposits made to the bank
     uint256 public countDeposits = 0;
@@ -51,25 +66,28 @@ contract KipuBank is AccessControl {
     /// @notice Total number of withdraws made from the bank
     uint256 public countWithdraws = 0;
 
-    /// @notice Mapping to store the balance of each account
-    mapping(address => uint256) private balances; // ETH balances
+    /// @notice Per-user per-token balances
+    mapping(address => mapping(address => uint256)) private balances;
 
     /// =========================== EVENTS ===========================
 
     /// @notice Event emitted when a deposit is made
     /// @param account The address of the account making the deposit
-    /// @param value The amount of Ether deposited in wei
-    event Deposit(address indexed account, uint256 value);
+	/// @param token The address of the token deposited (ETH address is 0x0)
+    /// @param amount The amount of Ether deposited in wei
+    event Deposit(address indexed account, address indexed token, uint256 amount);
 
     /// @notice Event emitted when a withdraw is made
     /// @param account The address of the account making the withdrawal
+	/// @param token The address of the token withdrawn (ETH address is 0x0)
     /// @param value The amount of Ether withdrawn in wei
-    event Withdraw(address indexed account, uint256 value);
+    event Withdraw(address indexed account, address indexed token, uint256 value);
 
     /// @notice Emitted whenever an admin adjusts a user’s internal balance.
     event BalanceAdjusted(
         address indexed admin,
         address indexed account,
+		address indexed token,
         uint256 previousBalance,
         uint256 newBalance,
         int256 capDelta // +X means cap increased (debited user), -X means cap decreased (credited user)
@@ -83,7 +101,12 @@ contract KipuBank is AccessControl {
     /// @notice Bank capacity exceeded
     /// @param requested The amount requested to deposit
     /// @param available The available capacity in the bank
-    error BankCapExceeded(uint256 requested, uint256 available);
+    error BankCapEthExceeded(uint256 requested, uint256 available);
+
+	/// @notice Bank capacity exceeded for USDC
+	/// @param requested The amount requested to deposit
+	/// @param available The available capacity in the bank
+	error BankCapUsdcExceeded(uint256 requested, uint256 available);
 
     /// @notice Insufficient balance for withdraw
     /// @param requested The amount requested to withdraw
@@ -104,6 +127,9 @@ contract KipuBank is AccessControl {
 	/// @notice Oracle data is stale
 	error OracleStale(uint256 updateAt, uint256 nowTs);
 
+	/// @notice Unsupported token error
+	error UnsupportedToken(address token);
+
     /// =========================== MODIFIERS ===========================
 	modifier onlyAdminRole() {
 		_checkRole(DEFAULT_ADMIN_ROLE, msg.sender);
@@ -120,14 +146,31 @@ contract KipuBank is AccessControl {
     /// =========================== FUNCTIONS ===========================
 
     /// @notice Contract constructor
-    /// @param _maxBankCap The maximum capacity of the bank
-    constructor(uint256 _maxBankCap, address _ethUsdFeed) {
+    /// @param _maxBankCapEthWei The maximum capacity of the bank
+    constructor(uint256 _maxBankCapEthWei, uint256 _maxBankCapUsdc, address _ethUsdFeed, address _usdc) {
+		if (_maxBankCapEthWei == 0) {
+			revert InvalidValue();
+		}
+
+		if (_maxBankCapUsdc == 0) {
+			revert InvalidValue();
+		}
+
+		if (_usdc == address(0)) {
+			revert InvalidValue();
+		}
+
 		if (_ethUsdFeed == address(0)) {
 			revert OraclePriceInvalid();
 		}
 
-        MAX_BANK_CAP = _maxBankCap;
-        currentBankCap = MAX_BANK_CAP;
+        MAX_BANK_CAP_ETH = _maxBankCapEthWei;
+        currentBankCapEth = MAX_BANK_CAP_ETH;
+
+		MAX_BANK_CAP_USDC = _maxBankCapUsdc;
+		currentBankCapUsdc = MAX_BANK_CAP_USDC;
+
+		USDC = _usdc;
 
 		ethUsdFeed = AggregatorV3Interface(_ethUsdFeed);
 		feedDecimals = ethUsdFeed.decimals();
@@ -136,72 +179,109 @@ contract KipuBank is AccessControl {
         _grantRole(RECOVERY_ROLE, msg.sender);
     }
 
-    /// @notice The actual deposit function
-    function deposit() external payable onlyValidValue(msg.value) {
-        if (msg.value > currentBankCap) {
-            revert BankCapExceeded({
+    /// @notice The actual deposit ether function
+    function depositEth() external payable onlyValidValue(msg.value) {
+        if (msg.value > currentBankCapEth) {
+            revert BankCapEthExceeded({
                 requested: msg.value,
-                available: currentBankCap
+                available: currentBankCapEth
             });
         }
 
-        currentBankCap -= msg.value;
-        balances[msg.sender] += msg.value;
+        currentBankCapEth -= msg.value;
+        balances[msg.sender][ETH] += msg.value;
         incrementDepositCount();
 
-        emit Deposit(msg.sender, msg.value);
+        emit Deposit(msg.sender, ETH, msg.value);
     }
 
+	/// @notice The actual deposit USDC function
+	/// @param amount The amount of USDC to deposit
+	function depositUsdc(uint256 amount) external onlyValidValue(amount) {
+		IERC20(USDC).safeTransferFrom(msg.sender, address(this), amount);
+
+		// usdc has 6 decimals. If it has more, convert to 6 decimals
+		uint256 usdc = _stableToUsdc(USDC, amount);
+		if (usdc > currentBankCapUsdc) {
+			revert BankCapUsdcExceeded({
+				requested: usdc,
+				available: currentBankCapUsdc
+			});
+		}
+
+		currentBankCapUsdc -= usdc;
+		balances[msg.sender][USDC] += amount;
+		incrementDepositCount();
+
+		emit Deposit(msg.sender, USDC, amount);
+	}
+
     /// @notice The actual withdraw function
-    /// @param _value The amount of Ether to withdraw in wei
-    function withdraw(uint256 _value) external onlyValidValue(_value) {
-        if (_value > balances[msg.sender]) {
+	/// @param token The address of the token to withdraw (ETH address is 0x0)
+    /// @param _value The amount of token to withdraw
+    function withdraw(address token, uint256 _value) external onlyValidValue(_value) {
+		if (token != ETH && token != USDC) {
+			revert UnsupportedToken(token);
+		}
+
+		uint256 bal = balances[msg.sender][token];
+
+        if (_value > bal) {
             revert InsufficientBalance({
                 requested: _value,
-                available: balances[msg.sender]
+                available: bal
             });
         }
 
-        if (_value > ETHER_WITHDRAW_LIMIT) {
+        if (token == ETH && _value > ETHER_WITHDRAW_LIMIT) {
             revert WithdrawLimitExceeded({
                 requested: _value,
                 limit: ETHER_WITHDRAW_LIMIT
             });
         }
 
-		uint256 usdcAmount = _ethToUsdc(_value);
-		if (usdcAmount > WITHDRAW_LIMIT_USDC) {
+		uint256 usdcAmount = _toUsdc(token, _value);
+		if (usdcAmount > USDC_WITHDRAW_LIMIT) {
 			revert WithdrawLimitExceeded({
 				requested: usdcAmount,
-				limit: WITHDRAW_LIMIT_USDC
+				limit: USDC_WITHDRAW_LIMIT
 			});
 		}
 
-        balances[msg.sender] -= _value;
-        currentBankCap += _value;
+        balances[msg.sender][token] -= _value;
+
+		if (token == ETH) {
+			currentBankCapEth += _value;
+		} else {
+			currentBankCapUsdc += usdcAmount;
+		}
 		incrementWithdrawCount();
 
-        (bool success, ) = msg.sender.call{value: _value}("");
-        if (!success) {
-            revert TransferFailed();
-        }
+		if (token == ETH) {
+			(bool success, ) = msg.sender.call{value: _value}("");
+			if (!success) {
+				revert TransferFailed();
+			}
+		} else {
+			IERC20(USDC).safeTransfer(msg.sender, _value);
+		}
 
-        emit Withdraw(msg.sender, _value);
+        emit Withdraw(msg.sender, token, _value);
     }
 
     /// @notice Function to get the balance of a specific account
     /// @param account The address of the account to check the balance
-    function getBalance(address account) external view onlyAdminRole returns (uint256) {
-        return balances[account];
+    function getBalance(address account, address token) external view onlyAdminRole returns (uint256) {
+        return balances[account][token];
     }
 
     /// @notice Function to get the balance of the caller
-    function getBalance() external view returns (uint256) {
-        return balances[msg.sender];
+    function getMyBalance(address token) external view returns (uint256) {
+        return balances[msg.sender][token];
     }
 
-	function previewEthToUsdc(uint256 weiAmount) public view returns (uint256) {
-		return _ethToUsdc(weiAmount);
+	function previewToUsdc(address token, uint256 amount) public view returns (uint256) {
+		return _toUsdc(token, amount);
 	}
 
     function incrementDepositCount() private {
@@ -215,30 +295,52 @@ contract KipuBank is AccessControl {
     /// @notice Admin Recovery: set user's internal ETH balance.
 	/// @param account The address of the account to adjust
 	/// @param newBalance The new balance to set for the account
-    function setInternalBalance(address account, uint256 newBalance) external onlyRole(RECOVERY_ROLE) {
-        uint256 oldBalance = balances[account];
+    function setInternalBalance(address account, address token, uint256 newBalance) external onlyRole(RECOVERY_ROLE) {
+		if (token != ETH && token != USDC) {
+			revert UnsupportedToken(token);
+		}
+
+        uint256 oldBalance = balances[account][token];
 
         if (newBalance == oldBalance) {
-            emit BalanceAdjusted(msg.sender, account, oldBalance, newBalance, 0);
+            emit BalanceAdjusted(msg.sender, account, token, oldBalance, newBalance, 0);
             return;
         }
 
 		if (newBalance > oldBalance) {
 			uint256 delta = newBalance - oldBalance;
-			if (delta > currentBankCap) {
-				revert BankCapExceeded({
-					requested: delta,
-					available: currentBankCap
-				});
+
+			if (token == ETH) {
+				if (delta > currentBankCapEth) {
+					revert BankCapEthExceeded({
+						requested: delta,
+						available: currentBankCapEth
+					});
+				}
+				currentBankCapEth -= delta;
+				balances[account][ETH] = newBalance;
+				emit BalanceAdjusted(msg.sender, account, ETH, oldBalance, newBalance, -int256(delta));
+			} else {
+				uint256 usdc = _stableToUsdc(USDC, delta);
+                if (usdc > currentBankCapUsdc) revert BankCapUsdcExceeded(usdc, currentBankCapUsdc);
+                currentBankCapUsdc -= usdc;
+                balances[account][USDC] = newBalance;
+                emit BalanceAdjusted(msg.sender, account, USDC, oldBalance, newBalance, -int256(usdc));
 			}
-			currentBankCap -= delta;
-			balances[account] = newBalance;
-			emit BalanceAdjusted(msg.sender, account, oldBalance, newBalance, -int256(delta));
+
 		} else {
 			uint256 delta = oldBalance - newBalance;
-			currentBankCap += delta;
-			balances[account] = newBalance;
-			emit BalanceAdjusted(msg.sender, account, oldBalance, newBalance, int256(delta));
+
+			if (token == ETH) {
+				currentBankCapEth += delta;
+				balances[account][ETH] = newBalance;
+				emit BalanceAdjusted(msg.sender, account, ETH, oldBalance, newBalance, int256(delta));
+			} else {
+				uint256 usdc = _stableToUsdc(USDC, delta);
+				currentBankCapUsdc += usdc;
+				balances[account][USDC] = newBalance;
+				emit BalanceAdjusted(msg.sender, account, USDC, oldBalance, newBalance, int256(usdc));
+			}
 		}
     }
 
@@ -253,6 +355,13 @@ contract KipuBank is AccessControl {
     }
 
 	/// ========================== INTERNAL FUNCTIONS ===========================
+
+	/// @notice Internal function to convert ETH amount to USD with 6 decimals
+	/// @param token The address of the token (ETH or stablecoin)
+	/// @param amount The amount of ETH in wei
+    function _toUsdc(address token, uint256 amount) internal view returns (uint256) {
+        return (token == ETH) ? _ethToUsdc(amount) : _stableToUsdc(token, amount);
+    }
 
 	/// @notice Internal function to convert wei amount to USDC amount using Chainlink oracle
 	/// @param weiAmount The amount in wei to convert
@@ -275,9 +384,23 @@ contract KipuBank is AccessControl {
 		usdc = Math.mulDiv(weiAmount, scaledPrice, denom);
 	}
 
+	/// @notice Internal function to convert stablecoin amount to USDC amount
+	/// @param token The address of the stablecoin token
+	/// @param amount The amount of stablecoin to convert
+	function _stableToUsdc(address token, uint256 amount) internal view returns (uint256 usdc) {
+		uint8 d = IERC20Metadata(token).decimals();
+		if (d == 6) {
+			return amount;
+		} else if (d > 6) {
+			return amount / (10 ** (d - 6));
+		}
+
+		return amount * (10 ** (6 - d));
+	}
+
 	/// ========================== FALLBACK FUNCTION ===========================
 	/// @notice Fallback function to prevent direct ETH transfers
 	receive() external payable {
-		revert("Direct ETH not allowed; use deposit()");
+		revert("Direct ETH not allowed; use depositEth()");
 	}
 }
